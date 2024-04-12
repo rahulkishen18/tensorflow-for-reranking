cb91c68c9914a3bdea9bc39b2d731879affb508a
root <jennings@mosaicml.com>
2024-04-12 03:12:29 +0000

    fix

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# tensorflow-for-reranking
+## Launching TF jobs on MCLI
+
+This is a simple training workflow for training a neural reranker. It will:
+1. Automatically authenticate with GCP services based on credentials setup with MCLI
+2. Pull data from BigQuery
+3. Save checkpoints to GCP asynchronously
+4. Train a simple neural reranker
+
+### Some Background
+I spent most of the time wrangling with GCP services, mainly BigQuery. I'm not super familiar with the ins and outs of BigQuery, so wrangling the storing of features was most of the time.
+
+A nice benefit of MCLI is you can "ssh" (not exactly ssh protocol, but effectively exactly the same idea) onto a node and debug directly. I setup a Remote VS Code session on the node, clone down the repo and do some debugging there.
+
+To do that, you just run 
+```
+mcli interactive {NUMBER OF HOURS FOR INTERACTIVE SESSION} --cluster {CLUSTER YOU WANT TO USE}
+```
+
+Then you can play around directly on the box to your hearts content.
+
+### Provisioning environment for GCP authentication
+Our platform supports storing secure secrets per user, that are then injected into the users environment. Here I setup [GCP credentials](https://docs.mosaicml.com/projects/mcli/en/latest/resources/secrets/gcp.html) which are injected into the runtime in such a way that standard GCP Python libraries can use them to auto-authenticate. I know it says "GCP Storage", but this is a minsomer, it's for authenticating GCP in general.
+
+### Launching MCLI runs
+
+Once you've debugged your code, you can start launching runs that will spin up resources, execute a training run and tear down resources for you automatically. For this, you can just run 
+```
+mcli run -f tf-run.yaml
+```
+
+That's it!
--- main.py
+++ main.py
@@ -1,46 +1,114 @@
 import os
 from google.cloud import bigquery
 
+
+import threading
+
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import tensorflow_ranking as tfr
+from tensorflow_io.bigquery import BigQueryClient
+from tensorflow_io.bigquery import BigQueryReadSession
+from tensorflow.python.framework import dtypes
+import pdb
 
+# Uncomment for eager mode
+# tf.compat.v1.enable_eager_execution()
+# tf.data.experimental.enable_debug_mode()
 
+##### STEP 1 ###################
+##### Setup the environment ####
+################################
 # This can be set with `mcli create env secret GCP_PROJECT_ID=XXX`
 # You can create any arbitrary environment variable and have it automatically loaded
 # into a run with `mcli create env secret` (see: https://docs.mosaicml.com/projects/mcli/en/latest/resources/secrets/env.html)
-GCP_PROJECT_ID = os.environ['GCP_PROJECT_ID'] 
+GCP_PROJECT_ID = os.environ["GCP_PROJECT_ID"]
+DATASET_ID = "mslr_fold1"
+CHECKPOINT_PATH = "gs://mslr-connor/checkpoints/epoch"
+
 
 ## To make sure this is authed correctly, you'll want to run
 # `mcli create secret gcp` (see: https://docs.mosaicml.com/projects/mcli/en/latest/resources/secrets/gcp.html)
 # which will use GCP JSON credentials to authenticate
 client = bigquery.Client()
 
+
 client = bigquery.Client(project=GCP_PROJECT_ID)
-dataset = bigquery.Dataset(bigquery.dataset.DatasetReference(GCP_PROJECT_ID, 'mslr_fold1'))
-dataset.location = 'us'
+dataset = bigquery.Dataset(
+    bigquery.dataset.DatasetReference(GCP_PROJECT_ID, DATASET_ID)
+)
+dataset.location = "us"
 
-dataset = client.create_dataset(dataset)
+##### STEP 2 #####################
+##### Pull data from BigQuery ####
+##################################
+def read_bigquery(table_name):
+    tensorflow_io_bigquery_client = BigQueryClient()
+    read_session = tensorflow_io_bigquery_client.read_session(
+        parent="projects/" + GCP_PROJECT_ID,
+        project_id=GCP_PROJECT_ID,
+        table_id=table_name,
+        dataset_id=DATASET_ID,
+        selected_fields={
+            "features_flattened": {"output_type": dtypes.double, "mode": BigQueryClient.FieldMode.REPEATED},
+            "label": {"output_type": dtypes.double, "mode": BigQueryClient.FieldMode.REPEATED},
+            "query_id": {"output_type": dtypes.int64},
+            "doc_id":{"output_type": dtypes.int64, "mode": BigQueryClient.FieldMode.REPEATED},
+        },
+        requested_streams=2,
+    )
 
-# Prep data
-ds = tfds.load("mslr_web/10k_fold1", split="train")
-ds = ds.map(lambda feature_map: {
-    "_mask": tf.ones_like(feature_map["label"], dtype=tf.bool),
-    **feature_map
-})
-ds = ds.shuffle(buffer_size=1000).padded_batch(batch_size=32)
-ds = ds.map(lambda feature_map: (
+    dataset = read_session.parallel_read_rows()
+    return dataset
+
+
+BATCH_SIZE = 32
+training_ds = read_bigquery("mslr_fold1")
+training_ds = training_ds.map(lambda feature_map: {
+      "_mask": tf.ones_like(feature_map["label"], dtype=tf.bool),
+      **feature_map,
+      "float_features": tf.reshape(feature_map["features_flattened"], [-1, 136]) # 136 is the original length of each row in the original tensor, easier to digest in BigQuery
+  }
+)
+training_ds = training_ds.shuffle(buffer_size=1000).padded_batch(batch_size=BATCH_SIZE)
+training_ds = training_ds.map(lambda feature_map: (
     feature_map, tf.where(feature_map["_mask"], feature_map.pop("label"), -1.)))
 
+
+##### STEP 3 #####################
+##### Setup async cloud checkpointing ####
+##################################
+### This will async upload the checkpoint
+# Note: migrate to multiprocessing, this may not fully unblock the main loop
+class AsyncModelCheckpoint(tf.keras.callbacks.Callback):
+    def __init__(self, filepath):
+        super(AsyncModelCheckpoint, self).__init__()
+        self.filepath = filepath
+
+    def on_epoch_end(self, epoch, logs=None):
+        # Copy weights to avoid conflicts with ongoing training
+        weights = self.model.get_weights()
+        threading.Thread(target=self.save_weights, args=(weights, epoch)).start()
+
+    def save_weights(self, weights, epoch):
+        # Create a new model with the same architecture
+        model_clone = tf.keras.models.clone_model(self.model)
+        model_clone.set_weights(weights)
+        
+        # Save the cloned model's weights asynchronously
+        model_clone.save_weights(self.filepath.format(epoch=epoch), save_format='tf')
+        
+    
 # Create a model
-inputs = {
-    "float_features": tf.keras.Input(shape=(None, 136), dtype=tf.float32)
-}
+checkpoint_path = CHECKPOINT_PATH + "_{epoch:02d}"
+async_checkpoint_callback = AsyncModelCheckpoint(filepath=checkpoint_path)
+
+inputs = {"float_features": tf.keras.Input(shape=(None, 136), dtype=tf.float32)}
 norm_inputs = [tf.keras.layers.BatchNormalization()(x) for x in inputs.values()]
 x = tf.concat(norm_inputs, axis=-1)
 for layer_width in [128, 64, 32]:
-  x = tf.keras.layers.Dense(units=layer_width)(x)
-  x = tf.keras.layers.Activation(activation=tf.nn.relu)(x)
+    x = tf.keras.layers.Dense(units=layer_width)(x)
+    x = tf.keras.layers.Activation(activation=tf.nn.relu)(x)
 scores = tf.squeeze(tf.keras.layers.Dense(units=1)(x), axis=-1)
 
 # Compile and train
@@ -48,5 +116,9 @@ model = tf.keras.Model(inputs=inputs, outputs=scores)
 model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
     loss=tfr.keras.losses.SoftmaxLoss(),
-    metrics=tfr.keras.metrics.get("ndcg", topn=5, name="NDCG@5"))
-model.fit(ds, epochs=3)
+    metrics=tfr.keras.metrics.get("ndcg", topn=5, name="NDCG@5"),
+)
+##### STEP 3 #####################
+##### Train model ####
+##################################
+model.fit(training_ds, epochs=3, callbacks=[async_checkpoint_callback])
